34b8234335994b85c4a98c78f63401a14f10b4c4
Andrey Fomin <a48671@gmail.com>
2019-04-23 18:29:04 +0300

    build

---
+++ precache-manifest.88e6270d3c77f429d155816973e497b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3042cbd4f11bf97064ad",
+    "url": "/static/js/main.ad1fe162.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d916f9dbb49591c0ede6",
+    "url": "/static/js/2.05c03182.chunk.js"
+  },
+  {
+    "revision": "a8981df036c81f27b91e87190fa0a51b",
+    "url": "/index.html"
+  }
+];
